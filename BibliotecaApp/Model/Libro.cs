75b85e3ffa2d48e9fc8f5ecda7799d8f822b9f90
Cyn <sancyn10@github.com>
2025-04-29 21:49:49 -0300

    Creación de la clase Libro con sus atributos

---
+++ BibliotecaApp/Model/Libro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaApp.Model
+{
+    internal class Libro
+    {
+        private string codLibro;
+        private string titulo;
+        private string autor;
+        private string editorial;
+
+        public Libro(string codLibro, string titulo, string autor, string editorial)
+        {
+            this.CodLibro = codLibro;
+            this.Titulo = titulo;
+            this.Autor = autor;
+            this.Editorial = editorial;
+        }
+
+        public string CodLibro { get => codLibro; set => codLibro = value; }
+        public string Titulo { get => titulo; set => titulo = value; }
+        public string Autor { get => autor; set => autor = value; }
+        public string Editorial { get => editorial; set => editorial = value; }
+    }
+}
