faebcf8f664bebde23abdd1ea0dea5e234baba1c
Emilia Sosa <emi.sosato@gmail.com>
2025-05-01 20:53:08 -0300

    Se agregaron libros en la biblioteca

--- BibliotecaApp/Program.cs
+++ BibliotecaApp/Program.cs
@@ -2,10 +2,14 @@
 using BibliotecaApp.Model;
 Biblioteca biblioteca = new();
 
-
-
+biblioteca.AgregarLibro("La naranja mecanica", "Guille", "Ivrea");
+biblioteca.AgregarLibro("El tunel", "Cyn", "Ivrea");
+biblioteca.AgregarLibro("Martin Fierro", "Mati", "Ivrea");
 
 Console.WriteLine("Escribe el título del libro que quieres buscar: ");
 String titulo = Console.ReadLine();
 biblioteca.BuscarLibro(titulo);
+
+biblioteca.EliminarLibro("El tunel");
+
 Console.ReadKey();
